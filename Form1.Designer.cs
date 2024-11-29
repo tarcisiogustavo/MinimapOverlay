57e313b3054def1132d9e429dad9d148e459b06b
Tarcisio Gustavo <tarcisiogustavoo@gmail.com>
2024-11-29 16:53:28 -0300

    MinimapOverlay

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿using System.Windows.Forms;
+
+namespace MinimapOverlay
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.RichTextBox coordinatesTextBox;
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.LinkLabel label1;
+        private System.Windows.Forms.Button colorPickerButton;
+        private System.Windows.Forms.TrackBar transparencyTrackBar;
+        private System.Windows.Forms.Label transparencyLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.coordinatesTextBox = new System.Windows.Forms.RichTextBox();
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.LinkLabel();
+            this.colorPickerButton = new System.Windows.Forms.Button();
+            this.transparencyTrackBar = new System.Windows.Forms.TrackBar();
+            this.transparencyLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.transparencyTrackBar)).BeginInit();
+            this.SuspendLayout();
+
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+
+            this.coordinatesTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.coordinatesTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.coordinatesTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.coordinatesTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.coordinatesTextBox.ForeColor = System.Drawing.Color.White;
+            this.coordinatesTextBox.Location = new System.Drawing.Point(840, 68);
+            this.coordinatesTextBox.Name = "coordinatesTextBox";
+            this.coordinatesTextBox.Size = new System.Drawing.Size(300, 350);
+            this.coordinatesTextBox.TabIndex = 1;
+            this.coordinatesTextBox.Text = "";
+            this.coordinatesTextBox.ScrollBars = RichTextBoxScrollBars.Vertical;
+            this.coordinatesTextBox.WordWrap = false;
+            this.coordinatesTextBox.TextChanged += new System.EventHandler(this.CoordinatesTextBox_TextChanged);
+
+            this.instructionsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.instructionsLabel.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.instructionsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.instructionsLabel.Location = new System.Drawing.Point(840, 39);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(300, 25);
+            this.instructionsLabel.TabIndex = 2;
+            this.instructionsLabel.Text = "Coordenadas: ";
+
+            this.colorPickerButton.Anchor = AnchorStyles.Right;
+            this.colorPickerButton.BackColor = System.Drawing.Color.Red;
+            this.colorPickerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.colorPickerButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.colorPickerButton.ForeColor = System.Drawing.Color.White;
+            this.colorPickerButton.Location = new System.Drawing.Point(840, 430);
+            this.colorPickerButton.Name = "colorPickerButton";
+            this.colorPickerButton.Size = new System.Drawing.Size(140, 30);
+            this.colorPickerButton.TabIndex = 6;
+            this.colorPickerButton.Text = "Escolher Cor";
+            this.colorPickerButton.UseVisualStyleBackColor = false;
+            this.colorPickerButton.Click += new System.EventHandler(this.ColorPickerButton_Click);
+
+            this.transparencyTrackBar.Anchor = AnchorStyles.Right;
+            this.transparencyTrackBar.Location = new System.Drawing.Point(840, 470); 
+            this.transparencyTrackBar.Maximum = 255;
+            this.transparencyTrackBar.Minimum = 0;
+            this.transparencyTrackBar.Value = 180;
+            this.transparencyTrackBar.TickFrequency = 25;
+            this.transparencyTrackBar.Name = "transparencyTrackBar";
+            this.transparencyTrackBar.Size = new System.Drawing.Size(300, 45);
+            this.transparencyTrackBar.TabIndex = 7;
+            this.transparencyTrackBar.Scroll += new System.EventHandler(this.TransparencyTrackBar_Scroll);
+
+            this.transparencyLabel.Anchor = AnchorStyles.Right;
+            this.transparencyLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.transparencyLabel.ForeColor = System.Drawing.Color.White;
+            this.transparencyLabel.Location = new System.Drawing.Point(840, 520); 
+            this.transparencyLabel.Name = "transparencyLabel";
+            this.transparencyLabel.Size = new System.Drawing.Size(300, 20);
+            this.transparencyLabel.TabIndex = 8;
+            this.transparencyLabel.Text = "Transparência: 180";
+
+            this.exportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exportButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.exportButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.exportButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.exportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exportButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.exportButton.ForeColor = System.Drawing.Color.White;
+            this.exportButton.Location = new System.Drawing.Point(840, 560); 
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(300, 45);
+            this.exportButton.TabIndex = 3;
+            this.exportButton.Text = "Exportar Imagem";
+            this.exportButton.UseVisualStyleBackColor = false;
+            this.exportButton.Click += new System.EventHandler(this.ExportButton_Click);
+
+            this.calculateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.calculateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.calculateButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.calculateButton.ForeColor = System.Drawing.Color.White;
+            this.calculateButton.Location = new System.Drawing.Point(840, 620); 
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(300, 45);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calcular Zona Central e Escala";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(17, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Created by: arron_0n";
+            this.label1.LinkColor = System.Drawing.Color.FromArgb(23, 162, 184);
+            this.label1.ActiveLinkColor = System.Drawing.Color.FromArgb(40, 167, 69);
+            this.label1.VisitedLinkColor = System.Drawing.Color.Gray;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(40, 40, 40);
+            this.label1.Links.Add(12, 8, "https://github.com/tarcisiogustavo");
+            this.label1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Label1_LinkClicked);
+
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(1170, 880);
+            this.Controls.Add(this.transparencyLabel);
+            this.Controls.Add(this.transparencyTrackBar);
+            this.Controls.Add(this.colorPickerButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.instructionsLabel);
+            this.Controls.Add(this.coordinatesTextBox);
+            this.Controls.Add(this.pictureBox);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "MinimapOverlay";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.transparencyTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using SkiaSharp;
+
+namespace MinimapOverlay
+{
+    public partial class Form1 : Form
+    {
+        private SKBitmap currentImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private SKColor selectedColor = SKColors.Red.WithAlpha(100); 
+        private int transparency = 100; 
+
+        private void CoordinatesTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var input = coordinatesTextBox.Text;
+                var coordinates = ParseCoordinates(input);
+
+                currentImage = GenerateZoneImage(coordinates);
+
+                ShowImage(currentImage);
+            }
+            catch
+            {
+            }
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+            if (currentImage != null)
+            {
+                using (var saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "PNG Files|*.png";
+                    saveFileDialog.Title = "Exportar Imagem";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        using (var stream = File.OpenWrite(saveFileDialog.FileName))
+                        {
+                            currentImage.Encode(SKEncodedImageFormat.Png, 100).SaveTo(stream);
+                        }
+                        MessageBox.Show("Imagem exportada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nenhuma imagem gerada para exportar!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var input = coordinatesTextBox.Text;
+                var coordinates = ParseCoordinates(input);
+
+                var referenceZone = new SKPoint[]
+                {
+                    new SKPoint(-320.26f, 7362.8f),
+                    new SKPoint(-319.85f, 7406.23f),
+                    new SKPoint(-321.07f, 7454.1f),
+                    new SKPoint(-322.52f, 7462.43f),
+                    new SKPoint(-326.98f, 7471.4f),
+                    new SKPoint(-339.0f, 7485.05f),
+                    new SKPoint(-357.14f, 7500.03f),
+                    new SKPoint(-377.88f, 7514.02f),
+                    new SKPoint(-530.5f, 7615.46f),
+                    new SKPoint(-542.84f, 7596.83f),
+                    new SKPoint(-561.05f, 7573.32f),
+                    new SKPoint(-569.89f, 7562.73f),
+                    new SKPoint(-586.31f, 7542.06f),
+                    new SKPoint(-594.24f, 7529.08f),
+                    new SKPoint(-597.21f, 7520.39f),
+                    new SKPoint(-597.3f, 7507.44f),
+                    new SKPoint(-597.35f, 7486.97f),
+                    new SKPoint(-598.67f, 7483.44f),
+                    new SKPoint(-601.51f, 7481.64f),
+                    new SKPoint(-614.98f, 7481.39f),
+                    new SKPoint(-620.37f, 7482.38f),
+                    new SKPoint(-625.2f, 7485.44f),
+                    new SKPoint(-629.46f, 7487.9f),
+                    new SKPoint(-636.12f, 7489.47f),
+                    new SKPoint(-642.52f, 7488.0f),
+                    new SKPoint(-647.18f, 7484.24f),
+                    new SKPoint(-649.44f, 7480.55f),
+                    new SKPoint(-650.53f, 7464.12f),
+                    new SKPoint(-668.33f, 7424.54f),
+                    new SKPoint(-644.34f, 7414.67f),
+                    new SKPoint(-619.38f, 7412.28f),
+                    new SKPoint(-561.88f, 7418.21f),
+                    new SKPoint(-505.01f, 7420.01f),
+                    new SKPoint(-497.88f, 7401.87f),
+                    new SKPoint(-494.83f, 7398.66f),
+                    new SKPoint(-469.15f, 7380.21f),
+                    new SKPoint(-445.61f, 7373.16f),
+                    new SKPoint(-406.23f, 7372.72f),
+                    new SKPoint(-380.14f, 7372.17f),
+                    new SKPoint(-324.25f, 7363.75f)
+                };
+
+                float referenceXScale = 27.31034482758621f;
+                float referenceYScale = 19.925867507886424f;
+
+                var (xScale, yScale) = CalculateDynamicScale(coordinates, referenceZone, referenceXScale, referenceYScale);
+
+                float minX = coordinates.Min(coord => coord.X);
+                float maxX = coordinates.Max(coord => coord.X);
+                float minY = coordinates.Min(coord => coord.Y);
+                float maxY = coordinates.Max(coord => coord.Y);
+
+                float xCenter = (minX + maxX) / 2;
+                float yCenter = (minY + maxY) / 2;
+
+                //string result = $"Centro da Zona:\nX: {xCenter:F2}, Y: {yCenter:F2}\n\n" +
+                //                $"Escalas:\nXScale: {xScale:F2}, YScale: {yScale:F2}";
+
+                string result = string.Format(CultureInfo.InvariantCulture,
+                   "x = {0:F2}, y = {1:F2}, rotation = 0.0, xScale = {2:F2}, yScale = {3:F2}",
+                   xCenter, yCenter, xScale, yScale);
+
+                Clipboard.SetText(result);
+                MessageBox.Show(result, "Resultados do Cálculo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao calcular: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private (float xScale, float yScale) CalculateDynamicScale(SKPoint[] zone, SKPoint[] referenceZone, float referenceXScale, float referenceYScale)
+        {
+            (float width, float height) CalculateBoundingBox(SKPoint[] coordinates)
+            {
+                float minX = coordinates.Min(coord => coord.X);
+                float maxX = coordinates.Max(coord => coord.X);
+                float minY = coordinates.Min(coord => coord.Y);
+                float maxY = coordinates.Max(coord => coord.Y);
+
+                return (maxX - minX, maxY - minY);
+            }
+
+            var (refWidth, refHeight) = CalculateBoundingBox(referenceZone);
+            var (width, height) = CalculateBoundingBox(zone);
+
+            float scaleFactorX = refWidth / referenceXScale;
+            float scaleFactorY = refHeight / referenceYScale;
+
+            float xScale = width / scaleFactorX;
+            float yScale = height / scaleFactorY;
+
+            return (xScale, yScale);
+        }
+
+
+        private SKPoint[] ParseCoordinates(string input)
+        {
+            var matches = Regex.Matches(input, @"\((-?\d+\.?\d*),\s*(-?\d+\.?\d*)\)");
+
+            if (matches.Count == 0)
+            {
+                throw new Exception("Nenhuma coordenada válida foi encontrada.");
+            }
+
+            var culture = CultureInfo.InvariantCulture;
+
+            return matches.Cast<Match>()
+                          .Select(match => new SKPoint(
+                              float.Parse(match.Groups[1].Value, culture),
+                              float.Parse(match.Groups[2].Value, culture)
+                          )).ToArray();
+        }
+
+        private void ColorPickerButton_Click(object sender, EventArgs e)
+        {
+            using (var colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var chosenColor = colorDialog.Color;
+                    selectedColor = new SKColor(chosenColor.R, chosenColor.G, chosenColor.B, (byte)transparency);
+                    colorPickerButton.BackColor = chosenColor; 
+                }
+            }
+        }
+
+        private void TransparencyTrackBar_Scroll(object sender, EventArgs e)
+        {
+            transparency = transparencyTrackBar.Value;
+            selectedColor = new SKColor(selectedColor.Red, selectedColor.Green, selectedColor.Blue, (byte)transparency);
+            transparencyLabel.Text = $"Transparência: {transparency}";
+        }
+
+
+        private SKBitmap GenerateZoneImage(SKPoint[] zoneCoordinates)
+        {
+            int imageWidth = 1276;
+            int imageHeight = 1268;
+
+            float minX = zoneCoordinates.Min(coord => coord.X);
+            float maxX = zoneCoordinates.Max(coord => coord.X);
+            float minY = zoneCoordinates.Min(coord => coord.Y);
+            float maxY = zoneCoordinates.Max(coord => coord.Y);
+
+            float xWidth = maxX - minX;
+            float yHeight = maxY - minY;
+
+            float xScale = imageWidth / xWidth;
+            float yScale = imageHeight / yHeight;
+
+            float offsetX = (imageWidth - (xWidth * xScale)) / 2;
+            float offsetY = (imageHeight - (yHeight * yScale)) / 2;
+
+            var bitmap = new SKBitmap(imageWidth, imageHeight);
+            using (var canvas = new SKCanvas(bitmap))
+            {
+                canvas.Clear(SKColors.Transparent);
+
+                var fillPaint = new SKPaint
+                {
+                    Color = selectedColor,
+                    Style = SKPaintStyle.Fill,
+                    IsAntialias = true
+                };
+
+                var borderPaint = new SKPaint
+                {
+                    Color = SKColors.IndianRed,
+                    Style = SKPaintStyle.Stroke, 
+                    StrokeWidth = 3, 
+                    IsAntialias = true
+                };
+
+                var adjustedCoordinates = zoneCoordinates.Select(coord =>
+                    new SKPoint(
+                        offsetX + ((coord.X - minX) * xScale),
+                        offsetY + ((maxY - coord.Y) * yScale)
+                    )).ToArray();
+
+                var zonePath = CreatePolygonPath(adjustedCoordinates);
+
+                canvas.DrawPath(zonePath, fillPaint);
+
+                canvas.DrawPath(zonePath, borderPaint);
+            }
+
+            return bitmap;
+        }
+
+
+
+        private SKPath CreatePolygonPath(SKPoint[] points)
+        {
+            var path = new SKPath();
+            if (points.Length > 0)
+            {
+                path.MoveTo(points[0]);
+                for (int i = 1; i < points.Length; i++)
+                {
+                    path.LineTo(points[i]);
+                }
+                path.Close();
+            }
+            return path;
+        }
+
+        private void ShowImage(SKBitmap image)
+        {
+            if (image != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var resizedImage = image.Resize(new SKImageInfo(1276, 1268), SKFilterQuality.High);
+
+                    resizedImage.Encode(SKEncodedImageFormat.Png, 100).SaveTo(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    pictureBox.Image = System.Drawing.Image.FromStream(ms);
+                }
+            }
+        }
+
+        private void instructionsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Label1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = e.Link.LinkData.ToString(),
+                UseShellExecute = true 
+            });
+        }
+
+    }
+}
